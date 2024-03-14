3a96d949ff330c950cf2bb91cb19aae1e9eada0f
nordluma <maxnordlund.95@hotmail.com>
2024-03-14 20:08:11 +0200

    feat: token representation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Debug)]
+enum TokenType {
+    Integer,
+    Plus,
+    Eof,
+}
+
+enum TokenValue {
+    Integer(u8),
+    Plus(char),
+    None,
+}
+
+impl std::fmt::Display for TokenValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenValue::Integer(int) => write!(f, "{}", int),
+            TokenValue::Plus(c) => write!(f, "{}", c),
+            TokenValue::None => write!(f, "None"),
+        }
+    }
+}
+
+struct Token {
+    kind: TokenType,
+    value: TokenValue,
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let kind = &self.kind;
+        let value = &self.value;
+
+        write!(f, "Token({:?}, {})", kind, value)
+    }
+}
+
+impl Token {
+    fn new(kind: TokenType, value: TokenValue) -> Self {
+        Self { kind, value }
+    }
+}
