4920569c4d1465725d442a9bbcf194054d6d936f
nordluma <maxnordlund.95@hotmail.com>
2024-03-20 21:12:28 +0200

    docs: add documentation of the supported grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+//! # Rascal
+//!
+//! Rascal is a Pascal like interpreted language.
+//!
+//! # Grammar
+//!
+//! ```no_run
+//! program : compound_statement DOT
+//!
+//! compound_statement : BEGIN statement_list END
+//!
+//! statement_list : statement
+//!                | statement SEMI statement_list
+//!
+//! statement : compound_statement
+//!           | assignment_statement
+//!           | empty
+//!
+//! assignment_statement : variable ASSIGN expr
+//!
+//! empty :
+//!
+//! epxr : term ((PLUS | MINUS) term)*
+//!
+//! term : factor ((MUL | DIV) factor)*
+//!
+//! factor : PLUS factor
+//!        | MINUS factor
+//!        | INTEGER
+//!        | LPAREN expr RPAREN
+//!        | variable
+//!
+//! variable : ID
+//! ```
+
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
